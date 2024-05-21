20bcf81be205241351b6e02a4a5effbb32bcf426
Simon <simonchumacero26@gmail.com>
2024-05-21 19:25:24 -0400

    Agregar archivos de proyecto.

---
+++ DelegatePractice/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+partial class Program
+{
+    delegate int MathOperation(int a, int b);
+
+   static void Main(string[] args)
+    {
+        MathOperation operation;
+
+        operation = Add;
+        int addResult = operation(7, 11);
+        Console.WriteLine($"7 + 11 = {addResult}");
+
+        operation = Substract;
+        int subResult = operation(4, 3);
+        Console.WriteLine($" 4 - 3 = {subResult}");
+
+        // multicast
+        operation = Add;
+        operation += Substract;
+
+        Delegate[] delegateList = operation.GetInvocationList();
+        foreach (MathOperation ope in delegateList)
+        {
+            int result = ope(12, 4);
+            Console.WriteLine($"Result = {result}");
+        }
+
+
+    }
+
+
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    static int Substract(int a, int b)
+    {
+        return a - b;
+    }
+}
